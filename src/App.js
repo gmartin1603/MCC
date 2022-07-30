d0b534de65f19acd1f07fd7fd2f3574da7dee13f
G Martin <georgemartin1603@yahoo.com>
2022-07-29 19:38:54 -0500

    Landing bug fix

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import Contact from "./components/Contact";
 import Content from "./components/Content";
 import Header from "./components/Header";
 import './App.css'
+import {useNavigate} from 'react-router-dom'
+import { useEffect } from "react";
 
 function App() {
+  let navigate = useNavigate()
+  
+  useEffect(() => {
+    navigate('/services')
+  },[])
+
   const styles = {
     main:`flex flex-col w-screen h-screen bg-clearBlack overflow-hidden`,
     body:`max-h-[80vh] flex grow`,
